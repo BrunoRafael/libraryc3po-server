7e5018a23e1716876642e274512ac1174b84478b
BrunoRafael <bruno.rafael.vasconcelos@ccc.ufcg.edu.br>
2016-04-20 09:26:37 -0300

    Last modifications

--- database.py
+++ database.py
@@ -1,22 +1,28 @@
 # coding: utf-8
 
 from google.appengine.ext import ndb
+import datetime
+import logging
 
 class Comment(ndb.Model):
-	commentId: ndb.StringProperty()
-	text: ndb.StringProperty()
-
+	authorId= ndb.StringProperty()
+	text= ndb.StringProperty()
 
 #class User(ndb.Model):
 
 class Book(ndb.Model):
-    bookId = ndb.StringProperty()
-    title = ndb.StringProperty(default="sem texto")
-    authors = ndb.StructuredProperty(ndb.StringProperty(), repeated=True)
-    description = ndb.StringProperty()
-    imgUrl = ndb.StringProperty()
-    create_datetime = ndb.DateTimeProperty(auto_now_add=True)
-    comments = ndb.StructuredProperty(Comment, repeated=True)
+	bookId= ndb.StringProperty()
+	title = ndb.StringProperty(default="sem titulo")
+	authors = ndb.StringProperty(repeated=True)
+	description = ndb.StringProperty()
+	imgUrl = ndb.StringProperty()
+	create_datetime = ndb.DateTimeProperty(auto_now_add=True)
+	comments = ndb.StructuredProperty(Comment, repeated=True)
+
+	def to_dict(self, URLBASE=""):
+		data = super(Book, self).to_dict()
+		data["url"] = "%s/books/%s" % (URLBASE, self.bookId)
+		return data
 
 def saveBook(bookId, title, authors, description, imgUrl):
 	book = Book(id=bookId)
@@ -25,20 +31,31 @@ def saveBook(bookId, title, authors, description, imgUrl):
 	book.description = description
 	book.bookId= bookId
 	book.imgUrl = imgUrl
-	#message.create_datetime = dateTime.dateTime.now()
-	book.put()
+	book.create_datetime = datetime.datetime.now()
+	logging.debug(book)
+	book.put();
 
-def saveComment(bookId, text):
+def saveLibrary(library):
+	ndb.put_multi(library);
+
+def saveComment(bookId, authorId, text):
 	book = Book(id=bookId)
+	book.comments.append(Comment(authorId=authorId, text=text))
+	book.put();
 
-def getLibrary(hostUrl):
-	books = Book.query()
-    data = []
-    for book in books:
-        data.append({"url": "%s/books/%s" % (hostUrl, book.msgid)})
-	
-	return books
+def getLibrary():
+	return Book.query().fetch();
 
 def getBook(bookId):
-	book = Book()
-   
+	return Book.get_by_id(bookId)
+
+def removeAll(type):
+	if(type == 'Book'):
+		ndb.delete_multi(
+	    	Book.query().fetch(keys_only=True)
+		)
+def listAllBooks():
+	result = Book.query();
+	for book in result:
+		logging.info(book)
+   	
--- main.py
+++ main.py
@@ -17,13 +17,12 @@
 #
 
 import os
-import datetime
-import json
 
 import webapp2
 import jinja2
 
 import database
+import datetime
 import utils
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -36,63 +35,91 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 class DocApiRequest(webapp2.RequestHandler):
     def get(self):
-		values = {
-			'title': 'Documentation'
-		}
-		template = JINJA_ENVIRONMENT.get_template('doc.html')
-		self.response.write(template.render(values));
+    	print("Aqui0")
+    	values = {'title': 'Documentation'}
+    	template = JINJA_ENVIRONMENT.get_template('doc.html')
+    	self.response.write(template.render(values))
  
 class BookLibraryRequest(webapp2.RequestHandler):
    def get(self):
-   		books = database.getLibrary(self.request.host_url)
-        
+   		
+   		response = []
+   		URLBASE = self.request.host_url
+   		books = database.getLibrary()
+   		for b in books:
+   			bookDict = b.to_dict(URLBASE)
+   			bookDictEncoded = Utils.dataToJson(bookDict).encode('utf-8')
+   			response.append(bookDictEncoded)
+   		
+   		self.response.write(response)
 
 class BookRequest(webapp2.RequestHandler):
-    def get(self):
-    	pass
+    def get(self, bookId):
+    	book = database.getBook(bookId);
+        if book is None:
+            self.response.set_status(400)
+            self.response.write('{"msg":"Livro \'%s\' não encontrado", "error":404, "datetime":"%s"}' % (bookid, datetime.datetime.now().isoformat()))
+            return
+
+        URLBASE = self.request.host_url
+        bookDict = book.to_dict(URLBASE);
+        self.response.write(utils.dataToJson(bookDict).encode('utf-8'))
 
 class BookRemoveRequest(webapp2.RequestHandler):
     def delete(self):
+    	print("Aqui3")
     	pass
 
 class BookUpdateRequest(webapp2.RequestHandler):
     def put(self):
+    	print("Aqui4")
     	pass
 
 class BookAddRequest(webapp2.RequestHandler):
     def post(self):
+    	print("Aqui5")
     	pass
+
 class BookSearchRequest(webapp2.RequestHandler):
 	def get(self):
+		print("Aqui6")
 		pass
 
 class BookAddCommentRequest(webapp2.RequestHandler):
 	def post(self):
+		print("Aqui7")
 		pass
 
 class BookAddCommentRequest(webapp2.RequestHandler):
 	def post(self):
+		print("Aqui8")
 		pass
 
 class DeleteDatabaseRequest(webapp2.RequestHandler):
 	def post(self):
+		print("Aqui9")
 		pass
 
+class BookLogListRequest(webapp2.RequestHandler):
+	def get(self):
+		database.listAllBooks()
+
 class CleanDatabaseRequest(webapp2.RequestHandler):
-	def post(self):
-		pass
+	def get(self):
+		print("clean")
+		utils.generateDatabase();
+		self.response.write("Tudo salvo!");
 
 app = webapp2.WSGIApplication([
     ('/', DocApiRequest),
     ('/books', BookLibraryRequest),
-    ('/books/.*', BookRequest),
     ('/books/addBook', BookAddRequest),
+    ('/books/listAllBooks', BookLogListRequest),
     ('/books/updateBook', BookUpdateRequest),
     ('/books/removeBook', BookRemoveRequest),
     ('/books/searchBooks', BookSearchRequest),
     ('/books/addComment', BookAddCommentRequest),
 	('/books/delete/database', DeleteDatabaseRequest),
-	('/books/clean/database', CleanDatabaseRequest)
+	('/books/clean/database', CleanDatabaseRequest),
+	('/books/(.*)', BookRequest),
 ], debug=True)
-
-utils.generateDatabase();
